30bd697ccfd038801e2577fae65e73ccf559534f
cianeken <vic.sotoo@duoucuc.cl>
2026-09-07 09:50:42 -0300

    clases

---
+++ clases2/colecciones.java
@@ -0,0 +1,24 @@
+package clases2;
+
+public class colecciones {
+    public static void main(String[] args) {
+        String[] alumnos = new String[3];
+        alumnos[0] = "Diego marin" ;
+        alumnos[1] = "Pedro" ;
+        alumnos[2] = "Juan";
+        
+        Integer[] edades = new Integer[3];
+        edades[0] = 20;
+        edades[1] = 30;
+        edades[2] = 19;
+    
+
+        System.out.println("Datos de " + alumnos[0] + " edad " + edades[0]);
+        System.out.println("Datos de " + alumnos[1] + " edad " + edades[1]);
+        System.out.println("Datos de " + alumnos[2] + " edad " + edades[2]);
+        
+        
+    }
+
+    
+}
